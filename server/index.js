a0ae41f6fceca240c1e63672a1197f50759d1e8a
Tomas Simpson <tomas.simpson@tridentutilities.co.uk>
2022-09-29 16:46:01 +0100

    Created server and connected to db.

---
+++ server/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+  host: "tsg-db.cscjhbruzusj.eu-west-2.rds.amazonaws.com",
+  user: "admin",
+  password: "Xsw27xs4e$",
+  database: "tsg",
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/api/users", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const sql =
+    "INSERT INTO Users (Username, Password, PasswordHash, Salt, IsAdmin) VALUES (?,?,NULL,NULL,0)";
+  db.query(sql, [username, password], (err, res) => {
+    console.log(err);
+  });
+});
+
+app.get("/api/matches", (req, res) => {
+  const userId = req.query.userId;
+  const sql = `SELECT
+      m.Id as MatchId,
+      mg.Identifier as MatchGroup,
+      t1.Name as Team1,
+      t2.Name as Team2,
+      m.MatchTime,
+      umg.Id as UserMatchGuessId,
+      umg.Team1Goals,
+      umg.Team2Goals
+    FROM
+      Matches m
+      INNER JOIN MatchGroups mg ON mg.Id = m.MatchGroupId
+      INNER JOIN Teams t1 ON t1.Id = m.Team1Id
+      INNER JOIN Teams t2 ON t2.Id = m.Team2Id
+      LEFT JOIN UserMatchGuesses umg ON umg.MatchId = m.Id AND umg.UserId = ?
+    ORDER BY
+      mg.Id, m.Id`;
+
+  db.query(sql, [userId], (err, result) => {
+    res.send(result);
+  });
+});
+
+app.listen(3001, () => {
+  console.log("running on port 3001");
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Axios from "axios";
 
-function App() {
+const App = () => {
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
+  const [matches, setMatches] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/matches?userId=1").then((response) => {
+      console.log(response);
+      setMatches(response.data);
+    });
+  }, []);
+
+  const submit = () => {
+    // Axios.post("http://localhost:3001/api/users", {
+    //   username: username,
+    //   password: password,
+    // }).then(() => {
+    //   alert("successful insert");
+    // });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tournament Score Guess</h1>
+
+      <div className="form">
+        {matches.map((x) => {
+          return (
+            <div>
+              {x.Team1}
+              <span>
+                <input type="text" onChange={(e) => {}}></input>
+                {" - "}
+                <input type="text" onChange={(e) => {}}></input>
+              </span>
+              {x.Team2}
+            </div>
+          );
+        })}
+
+        {/* <label>Username</label>
+        <input
+          type="text"
+          name="username"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        ></input>
+        <label>Password</label>
+        <input
+          type="text"
+          name="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></input> */}
+
+        <button onClick={submit}>Submit</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
